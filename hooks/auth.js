e7e8695f6d22bb0d96d5a011c5dd8976c46dc1d0
AdnaneAD1 <sidiamadouadnane4@gmail.com>
2025-04-11 16:59:32 +0200

    Update frontd

--- hooks/auth.js
+++ hooks/auth.js
@@ -42,7 +42,7 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
         setStatus(null)
 
         return axios
-            .post('/login', props)
+            .post('/api/login', props)
             .then(() => mutate())
             .catch(error => {
                 if (error.response.status !== 422) throw error
